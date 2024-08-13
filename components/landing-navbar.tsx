65f4bfada608c2fb90dec35b1492dffb7bde0e27
Jay Tailor <86292618+Jaytailor15@users.noreply.github.com>
2024-08-14 00:30:41 +0530

    Update landing-navbar.tsx

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -24,7 +24,7 @@ export const LandingNavbar = () =>{
                     <Image
                     fill
                     alt="Logo"
-                    src="/Logo.svg"
+                    src="/Logo.png"
                     />
                 </div>
                 <h1 className={cn("text-2xl font-bold text-white",font.className)}>
